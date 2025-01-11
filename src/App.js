9f23cb34e18adf57ffe3bf73d51ab90ce524e67f
Cobap-B <Chugo.rochedreux@gmail.com>
2025-01-11 17:55:35 +0100

    Ajout compteur basique

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <h1>Hello world</h1>
+        <BasiqueText txt="Je suis étudiant en 2 ème année" />
+
+
+
+
+        <Compteur />
     </div>
   );
+}
+
+
+
+
+
+function BasiqueText(props){
+  return (
+    <>
+      <p>{props.txt}</p>
+    </>
+  )
+}
+
+
+
+function Compteur(){
+  //Bien sur un compteur pour un test c'est toujours bien
+  const [num, SetNum] = useState(0);
+
+  function add(){
+      SetNum(num + 1);
+  }
+  return(
+    <>
+      <button onClick={() => SetNum(num-1)}>-</button>
+      {num}
+      <button onClick={add}>+</button>
+    </>
+  )
+  
 }
 
 export default App;
