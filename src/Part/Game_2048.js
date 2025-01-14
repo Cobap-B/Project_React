471d53d1f532f16c4430c3398091614316901e6f
Cobap-B <Chugo.rochedreux@gmail.com>
2025-01-14 21:11:49 +0100

    2048 FIN

--- src/Part/Game_2048.js
+++ src/Part/Game_2048.js
@@ -4,15 +4,24 @@ import '../CSS/Game_2048.css'
 
 function Game_2048(){
 
-    const [table, SetTable] = useState([
-        [null, null, null, null],
-        [null, null, null, null],
-        [null, null, null, null],
-        [null, null, null, null],
-    ]);
+    const [table, SetTable] = useState(createTable());
 
-    function add(){
-        const newTable = [...table];
+    function createTable(){
+        const tab = [
+            [null, null, null, null],
+            [null, null, null, null],
+            [null, null, null, null],
+            [null, null, null, null],
+        ];
+
+        for (let index = 0; index < Math.round(Math.random()+1); index++) {
+            tab[Math.round(Math.random()*3)][Math.round(Math.random()*3)] = Math.round(Math.random()+1)*2;
+        }
+
+        return tab;
+    }
+
+    function add(newTable){
 
         let posibility = [];
         table.map((value, iX)=>{
@@ -20,33 +29,100 @@ function Game_2048(){
                 if (val===null) posibility.push([iX, iY]);
             })
         })
-        console.log(posibility);
-
         if (posibility.length>0){
             let cas = Math.round(Math.random() * (posibility.length-1));
 
-            newTable[posibility[cas][0]][posibility[cas][1]] = 2;
+            let nb = Math.round(Math.random()+1)*2
+            newTable[posibility[cas][0]][posibility[cas][1]] = nb;
 
             SetTable(newTable);
         }
     }
 
-    const handleKeyPress = (event) => {
-        if (event.code === "Space") { // Vérifie si la touche pressée est "Espace"
-            add();
+    function verif(newTable){
+        //On ajoute en premier
+        add(newTable);
+    }
+    function deplacer(row){
+        let compact = row.filter((val)=>{
+            return val !== null;
+        })
+        
+        for (let index = 0; index < compact.length-1; index++) {
+            if (compact[index] === compact[index + 1]) {
+                compact[index] *= 2; 
+                compact[index + 1] = null;
+            }
         }
+
+        compact = compact.filter((val)=>{
+            return val !== null;
+        })
+
+        let res = [];
+        for (let index = 0; index < 4; index++) {
+            if(index<=compact.length-1){
+                res[index]=compact[index];
+            }else{
+                res[index]=null;
+            }
+        }
+        return res;      
+    }
+
+
+    function move(direction){
+        const newTable = [...table];
+        switch (direction) {
+            case "ArrowLeft" || "Q":
+                for (let index = 0; index < 4; index++) {
+                    let col = newTable.map((c)=>{return c[index]});
+                    col = deplacer(col);
+                    col.forEach((val,r)=>{newTable[r][index]=val}); 
+                }
+                verif(newTable);
+                break;
+            case "ArrowRight" || "D":
+                for (let index = 0; index < 4; index++) {
+                    let col = newTable.map((c)=>{return c[index]});
+                    col = deplacer(col.reverse()).reverse();
+                    col.forEach((val,r)=>{newTable[r][index]=val}); 
+                }
+                verif(newTable);
+                break;
+            case "ArrowDown" || "S":
+                for (let index = 0; index < 4; index++) {
+                    let nt = deplacer(newTable[index].reverse()).reverse();
+                    nt.forEach((val,r)=>{newTable[index][r]=val}); 
+                }
+                verif(newTable);
+                break;
+            case "ArrowUp" || "Z":
+                for (let index = 0; index < 4; index++) {
+                    let nt = deplacer(newTable[index]);
+                    nt.forEach((val,r)=>{newTable[index][r]=val}); 
+                }
+                verif(newTable);
+                break;
+            default:
+                break;
+        }
+        SetTable(newTable);
+    }
+
+    const handleKeyPress = (event) => {
+        move(event.code);
     };
 
     useEffect(() => {
         window.addEventListener("keydown", handleKeyPress);
-
         return () => {
             window.removeEventListener("keydown", handleKeyPress); // pour le détruire
         };
     }, []);
 
     return (
-        <div className='controller'>
+        <div className='controller2048'>
             {table.map((value, iX)=>{
                 return value.map((val, iY)=>{
                     if (val!==null){
@@ -64,12 +140,11 @@ function Number(props){
     const style = {
         gridColumn: props.iX+1,
         gridRow: props.iY+1,
-        backgroundColor: "red",
+        backgroundColor: "#de0c0c",
         color: "black"  
     };
-
-    if (props.val%4===0){
-        style.backgroundColor = "black";
+    if (Math.log2(props.val/2)%2===0){
+        style.backgroundColor = "#000";
         style.color="white" ;       
     }
 
